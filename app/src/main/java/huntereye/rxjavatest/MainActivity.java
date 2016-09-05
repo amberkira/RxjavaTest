31beb5700170b1e4bac8b343a249d02b59382b6a
amberkira <355679971@qq.com>
2016-09-05 17:23:54 +0800

    LoginBean done unversal app done

--- app/src/main/java/huntereye/rxjavatest/MainActivity.java
+++ app/src/main/java/huntereye/rxjavatest/MainActivity.java
@@ -1,20 +1,56 @@
 package huntereye.rxjavatest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.functions.Func2;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private SharedPreferences LoginTaken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        query().flatMap(new Func1<List<String>, Observable<String>>() {
+            @Override
+            public Observable<String> call(List<String> strings) {
+                return Observable.from(strings);
+            }
+        }).scan(new Func2<String, String, String>() {
+            @Override
+            public String call(String s, String s2) {
+                return null;
+            }
+        })
+                .take(3)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.d("output", "s");
+                    }
+                });
 
+    }
 
+    public static Observable<List<String>> query(){
+        List<String> list = Arrays.asList("a","b","c","d");
+        return Observable.just(list,list);
     }
 
     @Override
@@ -23,7 +59,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-1
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
